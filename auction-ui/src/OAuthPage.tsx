e786c07b15c27f90c8337e2b6782607a8fbf673c
Veronika130 <Vmosijcuksk@gmail.com>
2026-03-13 11:40:00 +0200

    page css

--- auction-ui/src/OAuthPage.tsx
+++ auction-ui/src/OAuthPage.tsx
@@ -7,30 +7,32 @@ export default function OAuthPage() {
         window.location.href = `${API_BASE}/auth/google/login`;
     };
 
-
     return (
         <div className="oauth-page">
-            <div className="oauth-card">
-                <div className="oauth-header">
-                    <h1>Авторизація</h1>
-                    <p>
-                        Увійдіть у систему за допомогою вашого облікового запису Google
-                    </p>
+
+            <div className="auth-card">
+
+                <div className="auth-left">
+                    <div className="oauth-header">
+                        <h1>Авторизація</h1>
+                        <p>
+                            Увійдіть у систему за допомогою вашого облікового запису Google
+                        </p>
+                    </div>
+
+                    <div className="oauth-buttons">
+                        <button className="oauth-btn google-btn" onClick={handleGoogleLogin}>
+                            <span className="oauth-icon">G</span>
+                            <span>Увійти через Google</span>
+                        </button>
+                    </div>
                 </div>
 
-                <div className="oauth-buttons">
-                    <button className="oauth-btn google-btn" onClick={handleGoogleLogin}>
-                        <span className="oauth-icon">G</span>
-                        <span>Увійти через Google</span>
-                    </button>
-
-
+                <div className="auth-right">
+                    <img src="/img/2456073.jpg" alt="login" />
                 </div>
 
-                <div className="oauth-footer">
-
-                </div>
             </div>
+
         </div>
-    );
-}
+    );}
